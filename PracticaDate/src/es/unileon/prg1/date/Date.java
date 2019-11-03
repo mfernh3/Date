98ab0317db95fa24bc5c817659c3db82c8191f6c
mfernh3 <mfernh03@estudiantes.unileon.es>
2019-11-03 18:45:00 +0100

    Metodos isSame

--- PracticaDate/src/es/unileon/prg1/date/Date.java
+++ PracticaDate/src/es/unileon/prg1/date/Date.java
@@ -6,9 +6,9 @@ public class Date {
 	private int year;
 	
 	public Date(){
-		this.year = 
-		this.month =
-		this.day = 
+		this.year = 2019;
+		this.month = 0;
+		this.day = 0;
 		public Date(int day, int month, int year) throws DateException{
 			this.year = year;
 			if (month < 1 || month > 12) {
@@ -17,5 +17,23 @@ public class Date {
 			} else {
 				this.month = month;
 			}
+			switch(month) {
+			case 1:
+			case 3:
+			case 5:
+			case 7:
+			case 8:
+			case 10:
+			case 12:
+				if((day>=1) && (day<=31)){
+					this.day = day;	
+				} else {
+					throw new DateException("Dia " + day + " no valido para el mes " + month + ", valores posibles entre 1 y 31.");
+				}
+				break;
+			case 11:
+				
+			}
+		}
 	}
 }
