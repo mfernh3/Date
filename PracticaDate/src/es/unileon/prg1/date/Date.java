ce14a582e5a86c2c891872edc4d6b6abe1904699
mfernh3 <mfernh03@estudiantes.unileon.es>
2019-11-03 18:50:11 +0100

    isSame completos

--- PracticaDate/src/es/unileon/prg1/date/Date.java
+++ PracticaDate/src/es/unileon/prg1/date/Date.java
@@ -9,6 +9,7 @@ public class Date {
 		this.year = 2019;
 		this.month = 0;
 		this.day = 0;
+		
 		public Date(int day, int month, int year) throws DateException{
 			this.year = year;
 			if (month < 1 || month > 12) {
@@ -31,9 +32,98 @@ public class Date {
 					throw new DateException("Dia " + day + " no valido para el mes " + month + ", valores posibles entre 1 y 31.");
 				}
 				break;
+			case 4:
+			case 6:
+			case 9:
 			case 11:
-				
+				if((day>=1) && (day<=31)) {
+					this.day = day;
+				} else {
+					throw new DateException("Dia "+ day +"no valido para el mes " + month + ", valores posibles entre 1y 3");
+				}
+				break;
 			}
 		}
+		public Date(Date fecha){
+			this.day = fecha.getDay();
+			this.month = fecha.getMonth();
+			this.year = fecha.getYear();
+		}
+		
+		public String toString() {
+			return this.day + "/" + this.month + "/" + this.year;
+		}
+		
+		public int getDay{
+			return this.day;
+		}
+		
+		public int getMonth{
+			return this.month;
+		}
+
+		public int getYear{
+			return this.year;
+		}
+		
+		void setDay(int nuevoDay){
+			this.day = nuevoDay;	
+		}
+
+		void setMonth(int nuevoMonth){
+			this.month = nuevoMonth;
+		}
+		
+		void setYear(int nuevoYear){
+			this.year = nuevoYear;
+		}
+		
+		public boolean isSameYearIf(date another){
+			if ( this.year == another.getYear() ){
+				return true;
+			}
+			return false;
+		}
+		
+		public boolean isSameMonthIf(date another){
+			if ( this.month == another.getMonth() ){
+				return true;
+			}
+			return false;
+		}
+		
+		public boolean isSameDayIf(date another){
+			if ( this.day == another.getDay() ){
+				return true;
+			}
+			return false;
+		}
+		public boolean isSame(date another){
+			if (isSameDayIf(another) && isSameMonthIf(another)  && isSameYearIf(another)){
+				return true;
+			}
+			return false;
+		}
+		
+		public boolean isSameDay(Date another){
+			return another.day == this.day;
+		}
+		
+		public boolean isSameMonth(Date another){
+			return another.month == this.month;
+		}
+
+		public boolean isSameYear(Date another){
+			return another.year == this.year;
+		}
+
+		public boolean isSame(Date another){
+			return isSameDay(another) && isSameMonth(another)  && isSameYear(another);
+		}
+		
+		
+		
+		
+		
 	}
 }
