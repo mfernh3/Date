5c3abf12efbb6b4d47a7054f0271337d3b731c59
mfernh3 <mfernh03@estudiantes.unileon.es>
2019-11-03 18:21:45 +0100

    inicio

--- PracticaDate/src/es/unileon/prg1/date/Date.java
+++ PracticaDate/src/es/unileon/prg1/date/Date.java
@@ -1,5 +1,21 @@
 package es.unileon.prg1.date;
 
-
 public class Date {
+	private int day;
+	private int month;
+	private int year;
+	
+	public Date(){
+		this.year = 
+		this.month =
+		this.day = 
+		public Date(int day, int month, int year) throws DateException{
+			this.year = year;
+			if (month < 1 || month > 12) {
+				throw new DateException("Mes " + month + " no valido" +
+						" Valores posibles entre 1 y 12.");
+			} else {
+				this.month = month;
+			}
+	}
 }
